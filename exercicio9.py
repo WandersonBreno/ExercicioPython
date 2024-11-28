878e7d491901f3457328f70f3efbcd3e84b304d4
Wanderson <wanderbreno2@gmail.com>
2024-11-27 21:30:47 -0300

    exercicio9

--- exercicio9.py
+++ exercicio9.py
@@ -1,2 +1,6 @@
 #Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a 
 #temperatura em graus Celsius.C = 5 * ((F-32) / 9).
+
+Temp_F = int(input("Digite quantos graus em Fahrenheit: "))
+
+print ('Temperatura em Celsius: %.2f' %(5 * (Temp_F-32) / 9))
